7f0cae9631fdce289deeece724bfae6b39a530f0
Muhammad Hassan Raza <mhassanr715@gmail.com>
2026-09-09 16:15:33 +0500

    Time: 0 ms (100%), Space: 8.6 MB (51.72%) - LeetHub

---
+++ LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countCommas(int n) {
+
+        long long ans = 0;
+long long start = 1000;
+long long commas = 1;
+
+while (start <= n) {
+    ans += (n - start + 1) * commas;
+
+    start *= 1000;
+    commas++;
+ 
+}
+   return ans;
+    }
+};
